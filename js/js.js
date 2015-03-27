029ada2bd803e161fd510ab83ebe790abfbd52e6
Leandro Pires de Souza <leandro.pires.souza@gmail.com>
2015-03-27 12:04:02 -0400

    changing nav colors and adding arrow key handlers

--- js/js.js
+++ js/js.js
@@ -78,4 +78,25 @@ $(function(){
 		previous();
 	});
 	
+	$(document).keydown(function(e) {
+		switch(e.which) {
+			case 37: // left
+			break;
+
+			case 38: // up
+				previous();
+			break;
+
+			case 39: // right
+			break;
+
+			case 40: // down
+				next();
+			break;
+
+			default: return; // exit this handler for other keys
+		}
+		e.preventDefault(); // prevent the default action (scroll / move caret)
+	});
+	
 });
